ba635d265b00ab9cf27bd59ea10638fbd8b37a00
Sergei <serjvertelko@gmail.com>
2021-03-15 23:31:45 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  var minimum = array.reduce(function (accumulator, currentValue) {
+    if(accumulator < currentValue) {
+      return accumulator;
+    } else {
+      return currentValue;
+    }
+  });
+  return minimum;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  var maximum = array.reduce(function (accumulator, currentValue) {
+    if(accumulator > currentValue) {
+      return accumulator;
+    } else {
+      return currentValue;
+    }
+  });
+  return maximum;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  
+  var sum = array.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  });
+  return sum / array.length;
+};
